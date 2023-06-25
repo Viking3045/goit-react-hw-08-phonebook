7ef255f2f12246ebc39ea773c702f14bb5839161
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-06-25 16:10:35 +0300

    add changes

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { filterContact } from 'redux/filterSlice';
-const Filter = () => {
+// import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { filterContact } from 'redux/filterSlice';
+// const Filter = () => {
 
-        const dispatch = useDispatch();
-  const filter = useSelector(state => state.filter.value);
+//         const dispatch = useDispatch();
+//   const filter = useSelector(state => state.filter.value);
 
-  const updateFilter = e => {
-    dispatch(filterContact(e.currentTarget.value));
-  }
+//   const updateFilter = e => {
+//     dispatch(filterContact(e.currentTarget.value));
+//   }
   
-    return (
-       <div>
-        <h2>Find contacts by name</h2>
-        <input onChange={updateFilter}
-           value={filter}/>
+//     return (
+//        <div>
+//         <h2>Find contacts by name</h2>
+//         <input onChange={updateFilter}
+//            value={filter}/>
         
-    </div>
-    )
-}
+//     </div>
+//     )
+// }
 
-export default Filter 
+// export default Filter 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
-
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react'
+// import { persistor , store } from './redux/store'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
   <React.StrictMode>
+    <Provider store={store}>
+{/* <PersistGate loading={null} persistor={persistor}> */}
+  <BrowserRouter basename="goit-react-hw-08-phonebook">
     <App />
+        </BrowserRouter>
+        {/* </PersistGate> */}
+        </Provider>
   </React.StrictMode>
-  </Provider>
 
 );
